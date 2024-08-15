e631a901843ace7d070d4f50e2e9bef1a4df727d
haans-mertens <haans.mertens@gmail.com>
2024-08-15 15:41:30 +0200

    Update 01 Collect frontpages.py

--- _pages/01 Collect frontpages.py
+++ _pages/01 Collect frontpages.py
@@ -1,3 +1,11 @@
+---
+permalink: /code_script1/
+title: "01 Collect frontpages.py"
+author_profile: false
+redirect_from: 
+  - /code_script1.html
+---
+
 import pandas as pd
 import numpy as np
 import requests
